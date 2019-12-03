207cd02048646221dade78c0f4505d056e906399
tysonlander <tysonlander@gmail.com>
2019-12-02 21:22:47 -0700

    Functiong: added form

--- angular-js-playlist-master/app/app.js
+++ angular-js-playlist-master/app/app.js
@@ -8,27 +8,44 @@ myNinjaApp.controller('NinjaController', ['$scope', function ($scope) {
     $scope.ninjas.splice(removedNinja, 1)
   }
 
+  $scope.addNinja = function () {
+    $scope.ninjas.push({
+      name: $scope.newninja.name,
+      belt: $scope.newninja.belt,
+      rate: parseInt($scope.newninja.rate),
+      available: true
+    });
+
+    $scope.newninja.name = '';
+    $scope.newninja.belt = '';
+    $scope.newninja.rate = '';
+  }
+
   $scope.ninjas = [
     {
       name: 'yoshi',
       belt: 'Green',
       rate: 50,
-      available: true
+      available: true,
+      thumb: "https://images.unsplash.com/photo-1570500002694-6d4d641887a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
     }, {
       name: 'crystal',
       belt: 'Yellow',
       rate: 30,
-      available: true
+      available: true,
+      thumb: "https://images.unsplash.com/photo-1570500002694-6d4d641887a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
     }, {
       name: 'ryu',
       belt: 'Orange',
       rate: 45,
-      available: true
+      available: true,
+      thumb: "https://images.unsplash.com/photo-1570500002694-6d4d641887a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
     }, {
       name: 'shaun',
       belt: 'Black',
       rate: 20,
-      available: true
+      available: true,
+      thumb: "https://images.unsplash.com/photo-1570500002694-6d4d641887a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"
     }];
 }]);
 
